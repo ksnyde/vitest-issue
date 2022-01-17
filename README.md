d449621baf70ba7f65402b361a3d469dae17dde0
ksnyde <ken@ken.net>
2022-01-16 23:13:27 -0800

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# vitest-dev-vitest-fsvsdz
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/vitest-dev-vitest-fsvsdz)
---
+++ src/App.vue
@@ -0,0 +1,15 @@
+<template>
+  <div>
+    <h1>Hello World</h1>
+    <div
+      data-region="switch"
+      class="a b c"
+      style="width: 500px; height: 500px"
+    ></div>
+  </div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue';
+export default defineComponent({ name: 'App' });
+</script>
